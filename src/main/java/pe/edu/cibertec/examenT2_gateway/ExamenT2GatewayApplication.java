b5321113b270786ff81f2b199285c3f23e459b24
Natha23XO <hugocastroleon2301@gmail.com>
2024-10-11 23:01:12 -0500

    subiendo

---
+++ src/main/java/pe/edu/cibertec/examenT2_gateway/ExamenT2GatewayApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.examenT2_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenT2GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenT2GatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/examenT2_gateway/ExamenT2GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.examenT2_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenT2GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
